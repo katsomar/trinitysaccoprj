8b2a44baacc2f351c5f3978f01c19fd943fa71ec
katsomar <katsomar60@gmail.com>
2025-07-19 15:08:19 +0300

    changed the app.jsx nav bar settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,26 +12,42 @@ import GroupView from './pages/GroupView';
 import Chat from './pages/Chat';
 import CustomerService from './pages/CustomerService';
 
+// Helper to conditionally render Navbar/Footer
+const Layout = ({ children }) => {
+    const location = useLocation();
+    const showLayout = [
+        '/',
+        '/about',
+        '/login',
+        '/signup',
+        '/customer-service'
+    ].includes(location.pathname);
+
+    return (
+        <div className="app">
+            {showLayout && <Navbar />}
+            <main>{children}</main>
+            {showLayout && <Footer />}
+        </div>
+    );
+};
+
 const App = () => {
     return (
         <Router>
-            <div className="app">
-                <Navbar />
-                <main>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/saver-dashboard" element={<SaverDashboard />} />
-                        <Route path="/manager-dashboard" element={<ManagerDashboard />} />
-                        <Route path="/group/:groupId" element={<GroupView />} />
-                        <Route path="/chat/:groupId" element={<Chat />} />
-                        <Route path="/customer-service" element={<CustomerService />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/saver-dashboard" element={<SaverDashboard />} />
+                    <Route path="/manager-dashboard" element={<ManagerDashboard />} />
+                    <Route path="/group/:groupId" element={<GroupView />} />
+                    <Route path="/chat/:groupId" element={<Chat />} />
+                    <Route path="/customer-service" element={<CustomerService />} />
+                </Routes>
+            </Layout>
         </Router>
     );
 };
